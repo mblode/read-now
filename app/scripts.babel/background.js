6dafa94f2db32cd2e0bd038521e990851f5d4930
Matthew Blode <m.blode@gmail.com>
2016-06-15 20:37:45 +1000

    Add

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,24 +4,31 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onActiveChanged.addListener(function () {
-  chrome.tabs.query({
-    currentWindow: true, active: true
-  }, function(tabs){
-    var tabUrl = tabs[0].url;
-    var indexString = tabUrl.indexOf('chrome-extension://');
-
-    if (indexString != 0) {
-      localStorage.setItem('previewUrl', tabs[0].url);
-      var previewUrl = localStorage.getItem('previewUrl');
-      console.log(previewUrl);
-    }
-  });
-});
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({'url': chrome.extension.getURL('../popup.html')},function(tab) {
  });
 });
 
+function queryTabs () {
+  chrome.tabs.query({
+    currentWindow: true, active: true
+  }, function(tabs) {
+    var tabUrl = tabs[0].url;
+    var substrings = ['chrome-extension://', 'chrome://'];
+    if (substrings.some(function(v) { return tabUrl.indexOf(v) >= 0; })) {
+    } else {
+      localStorage.setItem('previewUrl', tabs[0].url);
+      var previewUrl = localStorage.getItem('previewUrl');
+      console.log(previewUrl);
+    }
+  });
+}
+
+chrome.tabs.onUpdated.addListener(function () {
+  queryTabs();
+});
+
+chrome.tabs.onActiveChanged.addListener(function () {
+  queryTabs();
+});
 
